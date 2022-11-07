43762e721d7e2ad5a4d65a9e9517f8957f21cd87
Victor Ventura <euventura@gmail.com>
2022-11-07 09:59:57 -0300

    second commit

--- tests/Drivers/v31Test.php
+++ tests/Drivers/v31Test.php
@@ -12,7 +12,7 @@ class V31Test extends TestCase
 
     public function testItShouldReadFileSuccess()
     {
-        $content = file_get_contents('../files/v31.txt');
+        $content = file_get_contents(__DIR__ .'/../files/Notfis-v31.txt');
 
         $v31 = new v31();
         $result = $v31->read($content);
@@ -30,30 +30,6 @@ class V31Test extends TestCase
         ];
 
         $this->assertEquals($expected, $result->getData());
-    }
-
-    public function testItShouldReadOnlyTheChunkSize()
-    {
-        $afdReader = new AfdReader(__DIR__ . '/../files/afd_test.txt', AfdReader::AFD, 0, 1);
-
-        $expected = [
-            'header' => [
-                'sequency' => '000000000',
-                'type' => 1,
-                'identityType' => 'CNPJ',
-                'identityNumber' => '17484689000170',
-                'cei' => '000000000000',
-                'name' => 'Devs Nerds',
-                'SerialNumber' => '00000000000000001',
-                'registryStartDate' => '01012016',
-                'registryEndDate' => '13042016',
-                'generationDate' => '13042016',
-                'generationTime' => ['hour' => 11, 'minute' => 37],
-            ],
-            'mark' => []
-        ];
-
-        $this->assertEquals($expected, $afdReader->getAll());
     }
 
 }
