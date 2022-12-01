a73480004e597ff2dc797000e934703fc0de58b8
Victor Ventura <euventura@gmail.com>
2022-12-01 13:48:03 -0300

    v31tivit test

--- src/Drivers/Ocorren/v50tivit.php
+++ src/Drivers/Ocorren/v50tivit.php
@@ -2,9 +2,9 @@
 
 namespace Uello\Txtello\Drivers\Ocorren;
 
-use Uello\Txtello\Drivers\Ocorren\v31 as OcorrenV31;
+use Uello\Txtello\Drivers\Ocorren\v31;
 
-class v50tivit extends OcorrenV31
+class v50tivit extends v31
 {
     protected $configFile = 'Ocorren/v50tivit.php'; 
 }
